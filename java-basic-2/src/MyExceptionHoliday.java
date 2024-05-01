7d7a6c00c6f78037db8a78e95ab41f648675622f
22fi009hide <22fi009@ms.dendai.ac.jp>
2024-05-01 15:05:12 +0900

    MyExceptionTest

---
+++ java-basic-2/src/MyExceptionHoliday.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MyExceptionHoliday {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		MyExceptionHoliday myE = new MyExceptionHoliday();
+		while (true) {
+			System.out.println("日にちを入力してください。");
+			Scanner scan = new Scanner(System.in);
+			Integer day = scan.nextInt();
+			try {
+				myE.test(day);
+			} catch (NoHolidayException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	void test(Integer day) throws NoHolidayException {
+		Integer holiday[] = { 3, 4, 5, 6, 11, 12, 18, 19, 25, 26 };
+		for (int i = 0; i < holiday.length; i++) {
+			if (day == holiday[i]) {
+				throw new NoHolidayException();
+			}
+		}
+	}
+}
